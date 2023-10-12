54e1493bfb4de8826640f62dbbce80b5b5f0bc72
JosephM <josephpinoy2@yahoo.com>
2023-10-11 22:32:18 -0500

    Delete data.jsx

--- src/data.jsx
+++
@@ -1,8 +0,0 @@
- export const data = [
-  { link: '/' , imgSrc : 'home.svg' , text: 'Home'},
-  { link: '/Breakfast' , imgSrc : 'breakfast.svg' , text: 'Desayuno'},
-  { link: '/Entree' , imgSrc : 'steak.svg' , text: 'Entree'},
-  { link: '/Dessert' , imgSrc : 'dessert.svg' , text: 'Pastel'},
-  { link: '/Drinks' , imgSrc : 'soda.svg' , text: 'Bebidas'}
-  
-]
