9911341c30a34d079f03149fed7d72bd8b394a7d
JosephM <josephpinoy2@yahoo.com>
2023-10-11 22:33:50 -0500

    Use Mirage

--- src/routes/ItemDetails.jsx
+++ src/routes/ItemDetails.jsx
@@ -1,42 +1,41 @@
 import React from 'react'
 import { useParams, Link} from 'react-router-dom'
-import {menu} from '../menu'
 import '../stylesheets/itemDetails.css'
 import {CartContext} from '../components/AddToCart'
 
 export default function ItemType(props){
+
   const {itemID} = useParams()
   const { addItem} = React.useContext(CartContext)
-  const myItem = menu.filter(item => item.id == itemID)[0]
-
-function addedItem(){
-  addItem()
-}
+  const [myItem, setMyItem] = React.useState(null)
 
   React.useEffect(()=>{
-    fetch('/api/menu')
-      .then(res=> res.json())
-      .then(data => console.log(data))
-      
-  },[])
-  
+    fetch(`/api/menu/${itemID}`)
+      .then(res=>res.json())
+      .then(data=> setMyItem(data.menu))
+  }, [])
+
+
   return(
     <div className='container-sm'>
-      <Link to=".." relative='path'><img id="backArrow"src="/svgs/backArrow.svg" alt="" /></Link>
-      <div className='row' id='img-container'><img id="item-img" src={myItem.img} alt="" /></div>
-      <div className='row'>
-        <h3>{myItem.item}</h3>
-      </div>
-      <div className='row'>
-        <h6>${myItem.price}</h6>
-      </div>
-      <div  className='row'>
-        <p id='descr'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sapiente quae atque tempora nam aliquid deserunt ea, non debitis? Voluptatibus dolorum tenetur dolore? Dolorum, error?</p>
-      </div>
-      
+     {
+      myItem? ( <div>
+        <Link to=".." relative='path'><img id="backArrow"src="/svgs/backArrow.svg" alt="" /></Link>
+        <div className='row' id='img-container'><img id="item-img" src={myItem.img} alt="" /></div>
         <div className='row'>
-          <button className='btn btn-primary' onClick={addedItem}>Add to Cart</button>
+          <h3>{myItem.item}</h3>
         </div>
+        <div className='row'>
+          <h6>${myItem.price}</h6>
+        </div>
+        <div  className='row'>
+          <p id='descr'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sapiente quae atque tempora nam aliquid deserunt ea, non debitis? Voluptatibus dolorum tenetur dolore? Dolorum, error?</p>
+        </div>
+          <div className='row'>
+            <button className='btn btn-primary' onClick={ addItem}>Add to Cart</button>
+          </div>
+      </div>) : <h1>Loading...</h1>
+     }
     </div>
  )
 }
