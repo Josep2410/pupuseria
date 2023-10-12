61c33b8d341b14c1e39efe29edc31b05155882c0
JosephM <josephpinoy2@yahoo.com>
2023-10-12 14:18:49 -0500

    Updata ItemType.jsx
    Refactored to utilize miragejs

--- src/routes/ItemType.jsx
+++ src/routes/ItemType.jsx
@@ -1,36 +1,37 @@
 import React from 'react'
 import { useParams , Link} from 'react-router-dom'
-import {menu} from '../menu'
 import '../stylesheets/ItemType.css'
 
 export default function ItemType(props){
 
   const params = useParams()
   const category = params.itemType
+  const [filteredItems, setFilteredItems] = React.useState(null)
 
+  React.useEffect(()=>{
+    fetch('/api/menu')
+      .then(res=> res.json())
+      .then(data=>setFilteredItems(data.menus.filter(dish => dish.category == category)))
+  },[])
   
-  const items = menu.filter(dish => dish.category == category)
-
-  const displayItems = items.map(item=> {
-    return(
-      <Link to={`${item.id}`} key={item.id}>
-        <div className="grid-child ">
-          <img src={item.img} />
-          <div className='info-container container-sm'>
-            <p>{item.item}</p>
-            <span>${item.price}</span>
-          </div>
-         </div>
-      </Link>
-    )
-  })
-
-
   return(
   <>
     <h1 className="greeter">{params.itemType}</h1>
-    <div className="meal-container">
-      {displayItems}
-    </div>
+    {filteredItems && (
+       <div className="meal-container">
+       {filteredItems.map(item=> {
+           return(
+              <Link to={`${item.id}`} key={item.id}>
+                <div className="grid-child ">
+                  <img src={item.img} />
+                  <div className='info-container container-sm'>
+                    <p>{item.item}</p>
+                    <span>${item.price}</span>
+                  </div>
+                  </div>
+              </Link>
+                    )
+                    })}
+        </div>) }
   </>)
 }
