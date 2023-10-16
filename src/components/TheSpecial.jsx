d21f19d99a777f3ba8c14bc0347a9ac09b2cfca7
JosephM <josephpinoy2@yahoo.com>
2023-10-16 17:28:06 -0500

    update TheSpecial.jsx
     fetch request to determine which data to display depending on the day of week

--- src/components/TheSpecial.jsx
+++ src/components/TheSpecial.jsx
@@ -3,17 +3,28 @@ import '../stylesheets/TheSpecial.css'
 import { Link } from 'react-router-dom'
 
 export default function TheSpecial(props){
+
+  const d = new Date()
+
+  const [state, setState] = React.useState(null)
+
+  React.useEffect(()=>{
+    fetch(`/api/discounts/${d.getDay()}`)
+      .then(res =>res.json())
+      .then(data=> setState(data.discount))
+  },[])
+
   return(
-  <section id="theSpecial" className="container-sm">
-   <h1>La Especial</h1>
-   <Link to='/Entree'>
-    <div id="special_content">
-       <img id="special_image"src="https://res.cloudinary.com/sagacity/image/upload/c_crop,h_2470,w_3600,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/allwayz_hungry_pupusas_houston_argvux.jpg" alt="" />
-       <p id="special_paragraph">25% de descuento al pedir dos platos de entree</p>
-    </div>
-   </Link>
-     
-   
-  </section>
+  <div>
+    {state && <section id="theSpecial" className="container-sm">
+     <h1>La Especial</h1>
+     <Link to='/Entree'>
+      <div id="special_content">
+         <img id="special_image"src={state.img} alt="" />
+         <p id="special_paragraph">{state.discount}</p>
+      </div>
+     </Link>
+    </section>}
+  </div>
     )
 }
