b319e3d2aba7de5eff83acc6661140ec12e7e49d
JosephM <josephpinoy2@yahoo.com>
2023-10-17 20:22:41 -0500

    Update MyCart.jsx
    Update file to be more cohesive

--- src/routes/MyCart.jsx
+++ src/routes/MyCart.jsx
@@ -1,46 +1,49 @@
 import React from 'react'
-import { CartContext } from '../components/AddToCart'
+import { CartContext} from '../components/AddToCart'
 import '../stylesheets/cartItem.css'
-
+import {getDiscount_Tax_Total, getSubtotal} from '../utils/utils'
 
 export default function MyCart(){
 
  const {numOfItems, addItem, removeItem} = React.useContext(CartContext)
- let total = React.useRef(0)
+ const [cartItems, setCartItems] = React.useState(null)
+ const d = new Date()
+ const dayOfWeek = d.getDay()
+ let displayItems = []
+ let subTotal = 0;
+ let numsObj = {} //holds the discount, tax, and total info
 
- const [displayItems, setDisplayItems] = React.useState(null)
+ React.useEffect(()=>{
+  fetch(`/api/cartItems`)
+    .then(res=>res.json())
+    .then(data => setCartItems(data.menus))
+ }, [numOfItems])
 
-
-  React.useEffect(()=>{
-    fetch('/api/menu')
-      .then(res=>res.json())
-      .then(data => {
-        total.current = 0
-        setDisplayItems(data.menus
-          .filter(item=>item.inCart) //filter the array returned from fetch
-          .map(item=> { //then map over each item to render JSX
-            total.current = total.current + (item.numInCart * item.price)
-            return(
-              <div key={item.id} className='cartItem'>
-                <img src={`${item.img}`} alt="" id='cartImage'/>
-                <div id='cartInfo'>
-                  <h6 className={item.item.length > 22? 'small': 'standard'}>{item.item}</h6>
-                    <div className='flexInfo'>
-                      <span>Qty:</span>
-                      <span className='btnLayout flexInfo' >
-                        <span className='btn-operation' onClick={()=>increaseItemCount(item.id)}>+</span>
-                        <span >{item.numInCart > 0? item.numInCart: 0}</span>
-                        <span className={`btn-operation `} onClick={()=>decrementItemCount(item.id)}>-</span>
-                      </span>
-                    </div>
-                    <div className='flexInfo'><span>Price: </span> <span >${item.price }</span></div>
-                </div>
-              </div>
-          )
-        }))})
-  },[numOfItems])
-
-
+ if(cartItems){
+  subTotal = getSubtotal(cartItems)
+ numsObj = {...getDiscount_Tax_Total(6,cartItems, subTotal)}
+  displayItems = cartItems.map(item=> { //then map over each item to render JSX
+    return(
+      <div key={item.id} className='cartItem'>
+        <img src={`${item.img}`} alt="" id='cartImage'/>
+        <div id='cartInfo'>
+          <h6 className={item.item.length > 22? 'small': 'standard'}>{item.item}</h6>
+            <div className='flexInfo'>
+              <span>Qty:</span>
+              <span className='btnLayout flexInfo' >
+                <span className='btn-operation' onClick={()=>increaseItemCount(item.id)}>+</span>
+                <span >{item.numInCart > 0? item.numInCart: 0}</span>
+                <span className={`btn-operation `} onClick={()=>decrementItemCount(item.id)}>-</span>
+              </span>
+            </div>
+            <div className='flexInfo'><span>Price/Item </span> <span >${(item.price).toFixed(2)}</span></div>
+            <div className='flexInfo'><span>Price: </span> <span >${(item.price * item.numInCart).toFixed(2) }</span></div>
+        </div>
+      </div>
+  )
+})
+ }
+  
 function increaseItemCount(id){
   addItem()
   fetch(`/api/${id}`, {
@@ -62,17 +65,22 @@ function decrementItemCount(id){
   })
 }
 
+
   return(
   <div className='container-sm'>
     {numOfItems > 0? (
-        <form action="">
-       {displayItems}
-       <br />
-       <div className='divider col-12'></div>
-       <div className='flexInfo'><span >Subtotal : </span> <span>${total.current}</span></div>
-       <br />
-        <button className='btn btn-primary col-12'>Submit Order</button>
-    </form>
+      <form action="">
+        {displayItems}
+        <br />
+        <div className='flexInfo'><span >Subtotal : </span> <span>${(subTotal)}</span></div>
+        <div className='flexInfo'><span >Discount : </span> <span>- ${numsObj.discount || "0.00" }</span></div>
+        <div className='flexInfo'><span >Taxes : </span> <span>${(numsObj.tax)}</span></div>
+        <br />
+        <div className='divider col-12'></div>
+        <div className='flexInfo total'><span >Total : </span> <span>${numsObj.total}</span></div>
+        <br />
+          <button className='btn btn-primary col-12'>Submit Order</button>
+      </form>
     ) : <h1>Your Cart is Empty</h1>}
   
   </div>)
