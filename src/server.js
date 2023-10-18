45e4a3ad172d65a5272f2c99910d8957cf1d82bd
JosephM <josephpinoy2@yahoo.com>
2023-10-18 15:24:32 -0500

    change discount strings & rates

--- src/server.js
+++ src/server.js
@@ -133,7 +133,7 @@ createServer({
       numInCart : 0,
       item : 'Tacos mexicanos (3)',
       subOptions : ['Al Pastor', 'Barbacoa' , 'Birria' , 'Carne Asada', 'Carnitas', 'Chorizo', 'Pollo', 'Pescado'] ,
-      price : '4.99',
+      price : 4.99,
       img : 'https://bpprivilegeclub.com/blog/wp-content/uploads/2021/07/bigstock-three-carne-asada-mexican-stre-385360028-scaled.jpg'
     })
     server.create('menu', {
@@ -142,7 +142,7 @@ createServer({
       inCart : false,
       numInCart : 0,
       item : 'Fresco Horchata',
-      price : '1.99',
+      price : 1.99,
       img: 'https://travelfoodatlas.com/wp-content/uploads/2022/10/Fresh-Horchata-768x512.jpg.webp'
     })
     server.create('menu', {
@@ -151,7 +151,7 @@ createServer({
       inCart : false,
       numInCart : 0,
       item : 'Fresco Limonada',
-      price : '1.99',
+      price : 1.99,
       img : 'https://www.blossmangas.com/wp-content/uploads/2020/05/Lemonade-1-1.jpg'
     })
     server.create('menu', {
@@ -160,7 +160,7 @@ createServer({
       inCart : false,
       numInCart : 0,
       item : 'Agua Fresca',
-      price : '1.50',
+      price : 1.75,
       subOptions: ['sandia' , 'pepino' , 'pina', 'mango' , 'melon', 'tamarindo' ,'jamaica'],
       img : 'https://www.cookingclassy.com/wp-content/uploads/2021/06/aqua-fresca-5-600x400.jpg'
     })
@@ -170,7 +170,7 @@ createServer({
       inCart : false,
       numInCart : 0,
       item: 'agua',
-      price : '1.00',
+      price : .99,
       img : 'https://www.riverkeeper.org/wp-content/uploads/2018/04/bottled-water-credit-CC-Myrtle-Beach-TheDigitel-viaFlickr-modified-1000-650x333.jpg'
     })
     server.create('menu', {
@@ -179,7 +179,7 @@ createServer({
       inCart : false,
       numInCart : 0,
       item : 'coffee',
-      price : '2.50',
+      price : 2.50,
       img : 'https://media.cnn.com/api/v1/images/stellar/prod/150929101049-black-coffee-stock.jpg?q=x_3,y_1231,h_1684,w_2993,c_crop/h_540,w_960/f_webp'
     })
     server.create('menu', {
@@ -188,7 +188,7 @@ createServer({
       inCart : false,
       numInCart : 0,
       item : 'ice tea',
-      price : '1.99',
+      price : 1.99,
       img : 'https://sugarspunrun.com/wp-content/uploads/2022/05/Easy-sweet-tea-recipe-4-of-6.jpg'
     })
     server.create('menu',  {
@@ -197,7 +197,7 @@ createServer({
       inCart : false,
       numInCart : 0,
       item : 'Quesadilla',
-      price : '2.99',
+      price : 2.99,
       img : 'https://blog.amigofoods.com/wp-content/uploads/2020/09/salvadoran-quesdailla.jpg'
     })
     server.create('menu', {
@@ -206,7 +206,7 @@ createServer({
       inCart : false,
       numInCart : 0,
       item : 'Tres Leches',
-      price : '3.99',
+      price : 3.99,
       img : 'https://blog.amigofoods.com/wp-content/uploads/2019/07/Tres-leche-cake.jpg'
     })
     server.create('menu', {
@@ -215,7 +215,7 @@ createServer({
       inCart : false,
       numInCart : 0,
       item : 'Pastelitos de Pina',
-      price : '2.99',
+      price : 2.99,
       img : 'https://blog.amigofoods.com/wp-content/uploads/2023/03/pastelitos-de-pina.jpg'
     })
     server.create('menu', {
@@ -224,7 +224,7 @@ createServer({
       inCart : false,
       numInCart : 0,
       item : 'Torta Maria Luisa',
-      price: '2.99',
+      price: 2.99,
       img : 'https://blog.amigofoods.com/wp-content/uploads/2023/03/torta-maria-luisa.jpg'
     })
     server.create('menu', {
@@ -233,44 +233,46 @@ createServer({
       inCart : false,
       numInCart : 0,
       item : 'Semita',
-      price: '1.99',
+      price: 1.99,
       img: 'https://cdn0.recetasgratis.net/es/posts/9/1/8/pan_de_semitas_75819_600.webp'
     })
     server.create('discount', {
       id: 0,
      dayOfWeek : 'Domingo',
-     discount: "Todas las bebidas"
+     discount: "2 o Mas platos de desayuno 15 por ciento de descuento" // a coffee and dessert 20% off
     })
     server.create('discount', {
      id: 1,
      dayOfWeek : "Lunes",
-     discount: "25% de descuento al pedir dos platos de entree",
+     discount: "10% de descuento al pedir dos platos de entree",
      img: "https://res.cloudinary.com/sagacity/image/upload/c_crop,h_2470,w_3600,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/allwayz_hungry_pupusas_houston_argvux.jpg"
     })
     server.create('discount', {
       id: 2,
      dayOfWeek : "Martes",
-     discount: "Todas las bebidas"
+     discount: "Bebidas a mitad de precio",
+     img: "https://lavendervines.com/wp-content/uploads/2017/04/Agua-Fresca-1.jpg"
     })
     server.create('discount', {
       id: 3,
      dayOfWeek : "Miercoles",
-     discount: "Todas las bebidas"
+     discount: "Un cafe y postre 10 por ciento de descuento" //!!!!!!!!!!
     })
     server.create('discount', {
       id:4,
      dayOfWeek : "Jueves",
-     discount: "Todas las bebidas"
+     discount: "Un Plato principal y una bebida con 10 por ciento de descuento "
     })
     server.create('discount', {
       id: 5,
      dayOfWeek : "Viernes",
-     discount: "Todas las bebidas"
+     discount: "4 o Mas platos principales 10 por ciento de descuento" 
     })
     server.create('discount', {
       id: 6,
      dayOfWeek : "Sabado",
-     discount: "Todas las bebidas"
+     discount: "10 por ciento de descuento en platos principales y desayuno ",
+     img: "https://canalcocina.es/medias/_cache/zoom-ce2dc95fd5d8cc134220da1eaeb0ad51-920-518.jpg"
     })
   
   },
@@ -291,7 +293,18 @@ createServer({
       const id = request.params.id
       return schema.discounts.find(id)
     })
+    this.get('/cartItems', (schema, request)=>{
+      return schema.menus.where({inCart: true})
+    })
 
+    this.post(`/clearCart` , (schema, request)=>{
+      const menu = schema.menus.all()
+      menu.update({
+        inCart: false,
+        numInCart : 0
+      })
+      return menu
+    })
 
     this.post('/:id', (schema,request) =>{
       const id = request.params.id
