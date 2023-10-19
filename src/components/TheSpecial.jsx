8fb5ea309e47085602261f689ce0542f24f6cf20
JosephM <josephpinoy2@yahoo.com>
2023-10-18 23:13:31 -0500

    Fixed Special component 'Link to'

--- src/components/TheSpecial.jsx
+++ src/components/TheSpecial.jsx
@@ -5,20 +5,34 @@ import { Link } from 'react-router-dom'
 export default function TheSpecial(props){
 
   const d = new Date()
+  const day = d.getDay()
 
   const [state, setState] = React.useState(null)
 
   React.useEffect(()=>{
-    fetch(`/api/discounts/${d.getDay()}`)
+    fetch(`/api/discounts/${day}`)
       .then(res =>res.json())
       .then(data=> setState(data.discount))
   },[])
+
+  function getLink(day){
+    switch( day){
+      case 0 : return "Desayuno"
+      case 2 : 
+      case 3 : return "Bebidas"
+      case 1 : 
+      case 4 : 
+      case 5 : 
+      case 6 : return "Entree"
+      default : return ""
+    }
+  }
 
   return(
   <div>
     {state && <section id="theSpecial" className="container-sm">
      <h1>La Especial</h1>
-     <Link to='/Entree'>
+     <Link to={`/${getLink(day)}`}>
       <div id="special_content">
          <img id="special_image"src={state.img} alt="" />
          <p id="special_paragraph">{state.discount}</p>
